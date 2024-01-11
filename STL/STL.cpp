0dd7149545feec329a9f71bbbe3b1725a2504ab7
CppKotlin <cscppkotlin@gmail.com>
2024-01-11 21:35:15 +0300

    Добавьте файлы проекта.

---
+++ STL/STL.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	vector<int>myVector = { 23,46,678,98,45 };
+	vector<int>::iterator it = myVector.begin();
+
+	auto it2 = myVector.begin();
+	for (auto it = myVector.begin(); it != myVector.end(); it++)
+	{
+		cout << *it << endl;
+	}
+
+	return 0;
+}
