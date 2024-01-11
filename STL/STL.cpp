b1a818243be188d60f0ef791c28c4653c23c428b
CppKotlin <cscppkotlin@gmail.com>
2024-01-11 22:05:53 +0300

    capacity

--- STL/STL.cpp
+++ STL/STL.cpp
@@ -4,13 +4,27 @@ using namespace std;
 
 int main()
 {
-	vector<int>myVector = { 23,46,678,98,45 };
-	vector<int>::iterator it = myVector.begin();
+	setlocale(LC_ALL, "ru");
 
-	auto it2 = myVector.begin();
-	for (auto it = myVector.begin(); it != myVector.end(); it++)
+	vector<int>myVector = { 23,46,678,98,45 };
+	myVector.reserve(9999);
+	myVector.push_back(45443);
+	myVector.push_back(9);
+	myVector.push_back(1);
+	myVector.push_back(21);
+	cout << "количество элементов в векторе = " << myVector.size() << endl;
+	myVector.push_back(9);
+	myVector.push_back(1);
+	myVector.push_back(21);
+	
+	
+	cout << "количество элементов в векторе = " << myVector.size() << endl;
+	cout << "capacity = " << myVector.capacity() << endl;
+	myVector.shrink_to_fit();
+	cout << "capacity = " << myVector.capacity() << endl;
+	for (int i=0; i<myVector.size(); i++)
 	{
-		cout << *it << endl;
+		cout << myVector[i] << endl;
 	}
 
 	return 0;
