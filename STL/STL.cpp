34cd5c0a4326574fb011b633719808539f64808d
CppKotlin <cscppkotlin@gmail.com>
2024-01-12 22:59:39 +0300

    iterator

--- STL/STL.cpp
+++ STL/STL.cpp
@@ -2,30 +2,54 @@
 #include <vector>
 using namespace std;
 
+//Итераторы
+
 int main()
 {
 	setlocale(LC_ALL, "ru");
 
-	vector<int>myVector = { 23,46,678,98,45 };
-	myVector.reserve(9999);
-	myVector.push_back(45443);
-	myVector.push_back(9);
-	myVector.push_back(1);
-	myVector.push_back(21);
-	cout << "количество элементов в векторе = " << myVector.size() << endl;
-	myVector.push_back(9);
-	myVector.push_back(1);
-	myVector.push_back(21);
-	
-	
-	cout << "количество элементов в векторе = " << myVector.size() << endl;
-	cout << "capacity = " << myVector.capacity() << endl;
-	myVector.shrink_to_fit();
-	cout << "capacity = " << myVector.capacity() << endl;
-	for (int i=0; i<myVector.size(); i++)
+	vector<int>myVector = {12,435,456,68,32,798,4332};
+	auto it = myVector.begin();
+	for (auto i=myVector.begin(); i != myVector.end(); i++)
 	{
-		cout << myVector[i] << endl;
+		cout << *i << endl;
+	}
+	//advance(it, 5);
+	//myVector.insert(it, 100);
+	//cout << "+++++++ INSERT ++++++" << endl;
+	//for (auto i = myVector.begin(); i != myVector.end(); i++)
+	//{
+	//	cout << *i << endl;
+	//}
+	//cout << "+++++++ Erase ++++++" << endl;
+	auto itErase = myVector.begin();
+	cout << *itErase << endl;
+	////itErase++;
+	//myVector.erase(itErase);
+	//for (auto i = myVector.begin(); i != myVector.end(); i++)
+	//{
+	//	cout << *i << endl;
+	//}
+
+	cout << "+++++++ Erase 0-2 ++++++" << endl;
+	myVector.erase(itErase,itErase+2);
+	cout << *itErase << endl;
+
+	for (auto i = myVector.begin(); i != myVector.end(); i++)
+	{
+		cout << *i << endl;
 	}
 
+	cout << *itErase  << endl;
+
+	//cout << *(it+3) << endl;
+	//advance(it, 3);
+	//cout << *it << endl;
+	//cout << myVector[2] << endl;
+	/*for (vector<int>::reverse_iterator it=myVector.rbegin(); it !=myVector.rend(); it++)
+	{
+		
+		cout << *it << endl;
+	}*/
 	return 0;
 }
