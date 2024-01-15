1eff5cae931dcceea0de6b9244e3477041fbe49e
CppKotlin <cscppkotlin@gmail.com>
2024-01-15 22:04:56 +0300

    queue      //Очередь

--- STL/STL.cpp
+++ STL/STL.cpp
@@ -1,71 +1,65 @@
 ﻿#include <iostream>
-#include <set>
+#include<list>
+#include <queue>
 using namespace std;
 
-//set      //Бинарное дерево
-//multiset
+//queue      //Очередь
+// адаптер для  контейнеров -  лист или дек
+//priority_queue // Очередь приоритетов -  -  vector или дек
+// 
+//				//queue      //Очередь
+//back	Возвращает ссылку на последний и наиболее недавно добавленный элемент в конец queue.
+//empty	Проверяет, является ли queue пустым.
+//front	Возвращает ссылку на первый элемент в начале queue.
+//pop	Удаляет элемент из начала queue.
+//push	Добавляет элемент в конец queue.
+//size	Возвращает количество элементов в контейнере queue.
 
 
 int main()
 {
 	setlocale(LC_ALL, "ru");
-	multiset<int> mMs = {54,6356,8,75,54,65}; // мультисет может хранить дубли
-	mMs.insert(555);
-	mMs.insert(555);
-	mMs.insert(555);
-	mMs.insert(555);
-	mMs.insert(555);
-	mMs.insert(555);
+	queue<int> q;
+	auto a = q._Get_container();		// по дефолту deque
+	queue<int,list<int>> qQ;			// теперь работаем с List
 
-	auto it1 = mMs.lower_bound(8); // поиск первого элемента не меньше данного 
-	auto it2 = mMs.upper_bound(8); // поиск первого элемента большего данного 
-	auto it3 = mMs.equal_range(8); // вернет предыдущую пару lower_bound + upper_bound
-
-	for (auto&& it : mMs)
-	{
-		cout << it << endl;
+	q.push(34);
+	q.push(21);
+	q.push(64);
+	q.push(37);
+	q.push(93);
+	cout << "front " << q.front()<< endl;
+	q.pop();
+	cout << "Pop + front " << q.front() << endl;
+	cout << "Back " << q.back() << endl;
+	cout << "while (!.empty()) "<< endl;
+	cout << "Количество элементов в очереде = " << q.size() << endl;
+	int i = 1;
+	while (!q.empty())
+	{	
+		cout << i <<" = " << q.front() << endl;
+		q.pop();
+		++i;
 	}
+	cout << "Количество элементов в очереде = " << q.size() << endl<<endl;
 
-	set<int> mySet;
-	for (int i = 0; i < 20; ++i)
+	//					Очередь приоритетов
+	cout << "Очередь приоритетов" << endl;
+	priority_queue<int> pq;
+	pq.push(49);
+	pq.push(12);
+	pq.push(83);
+	pq.push(38);
+	pq.push(27);
+	cout << "Количество элементов в очереде = " << pq.size() << endl;
+	i = 1;
+	while (!pq.empty())
 	{
-		mySet.insert(rand() % 100);
+		cout << i << " = " << pq.top() << endl;
+		pq.pop();
+		++i;
 	}
-	mySet.insert(10);
-	mySet.insert(32);
-	mySet.insert(1);
-	mySet.insert(46);
-	// удаляем числа:
-	//mySet.erase(1);
-	// можно посмотреть результат получилось ли удалить:
-	auto result= mySet.erase(1);
-	cout << "Результат удаления числа из дерева: " << result << endl;
-	// так же и для insert  -  только возвращается  итератор + булевый результат
-	auto res = mySet.insert(23);
-	cout << "Результат inserta : "<< res.second << endl;
-	
-
-	for (auto&& it : mySet)
-	{
-		cout << it << endl;
-	}
-
-	auto it = mySet.find(33); //find  ищет число 33 и возвращает итератор на него если такое есть
-
-	int val;
-	cout << "Введите число для поиска" << endl;
-	cin >> val;
-	cout << endl;
-	if (mySet.find(val) != mySet.end())
-	{
-		cout << "число " << val << " найдено" << endl;
-	}
-	else
-	{
-		cout << "число " << val << " отсутствует" << endl;
-	}
-
-
+	cout << "Количество элементов в очереде = " << pq.size() << endl;
 
 	return 0;
 }
