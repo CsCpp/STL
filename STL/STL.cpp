ec649435f908d9a7c7fabb09802f9e2c9e1c44be
CppKotlin <cscppkotlin@gmail.com>
2024-01-20 22:09:48 +0300

    //Алгоритмы // Поиск //std::copy,  - просто копирует // std::copy_if - применяет придекат

--- STL/STL.cpp
+++ STL/STL.cpp
@@ -6,13 +6,9 @@ using namespace std;
 
 								//Алгоритмы
 								// Поиск
-								//std::find,  std::find_IF, std::find_if_not
+								//std::copy,  - просто копирует
+								// std::copy_if - применяет придекат
 
-//бинарный предикат:
-bool MyPred(int a, int b)
-{
-	return a > b;
-}
 
 template<typename T>
 void printVc(T &vc) 
@@ -41,54 +37,12 @@ class Person
 int main()
 {
 	setlocale(LC_ALL, "ru");
-	vector<int> vc = { 21,95,23,74,52,61,41,10,80,12,30 };
-	printVc(vc);
-	
 
-	cout << "Поиск(find) " << endl;
-	auto it=find(vc.begin(), vc.end(),41);
-	if (it == vc.end()) { cout << " нету " << endl; }
-	else
-	{
-		cout << " есть " << endl;
-	}
-
-	cout << "Поиск(find_if) " << endl;
-	// для условия принимайет предикат
-	auto result = find_if(vc.begin(), vc.end(), [](int a) {
-		return a < 0;
-		});
-
-	if (result == vc.end()) { cout << " нету " << endl; }
-		else
-		{
-			cout << " есть " << endl;
-		}
-	cout << "Поиск(find_if_not) " << endl;
-	// для условия принимайет предикат
-	auto result2 = find_if_not(vc.begin(), vc.end(), [](int a) {
-		return a < 0;
-		});
-
-	if (result2 == vc.end()) { cout << " нету " << endl; }
-	else
-	{
-		cout << " есть " << endl;
-	} // поиск для вектора из int
-
-
-	//сортировка массива
-	//cout << "Массив" << endl;
-	//const int SIZE = 7;
-	//int arr[SIZE] = {52,14,30,82,17,92,31};
-	//printVc(arr);
-	//cout << "Массив после cортировки без компоратора" << endl;
-	//sort(arr,arr+SIZE);
-	//printVc(arr);
-	cout << "Поиск(find_if) для вектора состоящего из класса Person" << endl;
+	cout << "std::copy_if -> для вектора состоящего из класса Person" << endl;
 	vector<Person> people
 	{
 		Person("Alina",200),
+		Person("Dasha",251),
 		Person("Vasua",99),
 		Person("Petya",120),
 		Person("Dasha",18),
@@ -96,22 +50,22 @@ int main()
 		Person("Tanya",111),
 		Person()				// конструктор с параметрами по умолчанию
 	};
+
+	vector<Person> result;
+	copy_if(people.begin(), people.end(), back_inserter(result), [](const Person& p) 
+		{
+			return p.Score>100;
+		});
+	sort(result.begin(), result.end(), [](const Person &p1, const Person &p2)
+		{
+			return p1.Score < p2.Score;
+		});
+
 	cout << "\t ИМЯ:\t БАЛЛЫ:" << endl;
-	for (auto& a : people)
+	for (auto& a : result)
 	{
 		cout << "\t" << a.Name << "\t" << a.Score << endl;
 	}
-	auto ptr2=find_if(people.begin(), people.end(), [](const Person& p)
-			{
-				return p.Name=="petya";
-			});
-
-	if (ptr2 == people.end()) { cout << " нету " << endl; }
-	else
-	{
-		cout << " есть " << endl;
-	}
-
-
+	
 	return 0;
 }
