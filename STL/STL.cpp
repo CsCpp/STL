515f349e8396da3bfe8506bc41b5307091baf18b
CppKotlin <cscppkotlin@gmail.com>
2024-01-22 20:39:32 +0300

    //Алгоритмы //#include<numeric> // accumulate //сумма, произведение //массив в строку

--- STL/STL.cpp
+++ STL/STL.cpp
@@ -3,44 +3,71 @@
 #include<vector>
 #include<list>
 #include <string>
+#include<numeric>
 using namespace std;
 
 								//Алгоритмы
-								// наименьший элемент // min_element
-								//minmax_element
+								//#include<numeric>
+								// accumulate
+								//сумма, произведение
+								//массив в строку
 								
 
-class Person
-{
-public:
-	Person(string name = "Name", double score = 0)
-	{
-		this->Name = name;
-		this->Score = score;
-	}
-	bool operator()(const Person& p)  // предикат - возвращает булевое значение
-	{
-		return p.Score > 100;
-	}
-	string Name;
-	double Score;
-};
-
-
-template<typename T>
-void printVc(T& vc)
-{
-	for (auto&& e : vc)
-	{
-		cout << e << endl;
-	}
-}
+//class Person
+//{
+//public:
+//	Person(string name = "Name", double score = 0)
+//	{
+//		this->Name = name;
+//		this->Score = score;
+//	}
+//	bool operator()(const Person& p)  // предикат - возвращает булевое значение
+//	{
+//		return p.Score > 100;
+//	}
+//	string Name;
+//	double Score;
+//};
+//
+//
+//template<typename T>
+//void printVc(T& vc)
+//{
+//	for (auto&& e : vc)
+//	{
+//		cout << e << endl;
+//	}
+//}////
 int main()
 {
 	setlocale(LC_ALL, "ru");
+	cout << "sum:" << endl;
+	vector<int> v = { 4,21,56,21,1 };
+	auto result=accumulate(v.begin(),v.end(),0);
+	cout << result << endl;
+	cout << "произведение:" << endl;
+	auto res = accumulate(v.begin(), v.end(), 1, [](int a, int b) {
+		return a * b;
+		});
+	cout << res << endl;
 
-	vector<int> v = { 4,21,56,21,78,31,-54,29,-24,4,58,12,34 };
-	list<int> lst = {4,21,13,54,-1,68,10,93,15};
+	cout << "сумма четных" << endl;
+	auto res2 = accumulate(v.begin(), v.end(), 0, [](int a, int b) {
+		if(b%2==0)return a + b;
+		else return a;
+		});
+	cout << res2 << endl;
+
+	cout << "массив в строку" << endl;
+	int mas []  = { 4,21,56,21,1 };
+
+	auto res3 = accumulate(next(begin(mas)), end(mas), to_string(mas[0]), [](string a, int b) {
+		return a+'-' + to_string(b);
+		});
+	cout << res3 << endl;
+
+
+	/*list<int> lst = {4,21,13,54,-1,68,10,93,15};
 	const int size = 4;
 	int arr[size] = { 21,34,8,5 };
 	
@@ -58,7 +85,7 @@ int main()
 	auto iitr2 = minmax_element(lst.begin(), lst.end());
 	cout << "min \t" << *iitr2.first << "\t max \t" << *iitr2.second << endl;
 	auto iiitr2 = minmax_element(arr, arr + size);
-	cout << "min \t" << *iiitr2.first << "\t max \t" << *iiitr2.second << endl;
+	cout << "min \t" << *iiitr2.first << "\t max \t" << *iiitr2.second << endl;*/
 	//vector<Person> people
 	//{
 	//	Person("Alina",200),
@@ -76,7 +103,6 @@ int main()
 	//	cout << "\t" << a.Name << "\t" << a.Score << endl;
 	//}
 	//cout << "Применяем: remove p.Score > 100" << endl;
-
 	//cout << "Применяем: people.erase(res2, people.end());" << endl;
 	//people.erase(remove_if(people.begin(), people.end(), [](const Person& p)
 	//	{
@@ -87,13 +113,11 @@ int main()
 	//{
 	//	cout << "\t" << a.Name << "\t" << a.Score << endl;
 	//}
-
 	//cout << "удаляем пробелы в string" << endl;
 	//string str = "Текст   с  несколькими  пробелами";
 	//cout << str << endl;
 	//str.erase(remove(str.begin(), str.end(), ' '),str.end());
 
-	//cout << str << endl;
 
 	
 	return 0;
